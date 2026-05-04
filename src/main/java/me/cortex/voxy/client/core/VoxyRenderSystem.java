d2ff246f7a260aa87c819977d5ea8dba484866f2
Jordan Olivet <jordanolivet@gmail.com>
2026-05-04 19:42:31 +0200

    Fix near plane to far for small  vanilla render distance

--- src/main/java/me/cortex/voxy/client/core/VoxyRenderSystem.java
+++ src/main/java/me/cortex/voxy/client/core/VoxyRenderSystem.java
@@ -428,7 +428,7 @@ public class VoxyRenderSystem {
         return base.mulLocal(
                 makeProjectionMatrix(0.05f, Minecraft.getInstance().gameRenderer.getDepthFar()).invert(),
                 new Matrix4f())
-                .mulLocal(makeProjectionMatrix(VoxyClient.getOcclusionDebugState() <= 1 ? 16f : 0.1f, 16 * 3000));
+                .mulLocal(makeProjectionMatrix(VoxyClient.getOcclusionDebugState() <= 1 ? 1f : 0.1f, 16 * 3000));
     }
 
     private boolean frexStillHasWork() {
